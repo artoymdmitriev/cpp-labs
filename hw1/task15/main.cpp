6e969f8a0f9479dddb550351d7151fef6715d088
artoymdmitriev <admitriev5813@gmail.com>
2017-09-29 23:47:36 +0300

    Add task15

---
+++ hw1/task15/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by artyom on 28.9.17.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Char size is: " << sizeof(char) << std::endl;
+    std::cout << "Bool size is: " << sizeof(bool) << std::endl;
+    std::cout << "Int size is: " << sizeof(int) << std::endl;
+    std::cout << "Short int size is: " << sizeof(short int) << std::endl;
+    std::cout << "Long int size is: " << sizeof(long int) << std::endl;
+    std::cout << "Unsigned int size is: " << sizeof(unsigned int) << std::endl;
+    std::cout << "Float size is: " << sizeof(float) << std::endl;
+    std::cout << "Double size is: " << sizeof(double) << std::endl;
+    std::cout << "Long double size is: " << sizeof(long double) << std::endl;
+    return 0;
+}
