4f35a087408f32a69dad3ba3fdb2d56dcce4cc17
artoymdmitriev <admitriev5813@gmail.com>
2017-09-29 23:41:34 +0300

    Add task2

---
+++ hw1/task2/main.cpp
@@ -0,0 +1,43 @@
+//
+// Created by artyom on 27.9.17.
+//
+
+#include <iostream>
+
+double sum(double, double);
+double difference(double, double);
+double multiplication(double, double);
+double division(double, double);
+
+int main() {
+    double a, b;
+    std::cout << "Enter the first number: ";
+    std::cin >> a;
+    std::cout << "Enter the second number: ";
+    std::cin >> b;
+    std::cout << "Sum is: " << sum(a, b) << std::endl;
+    std::cout << "Difference is: " << difference(a, b) << std::endl;
+    std::cout << "Multiplication is: " << multiplication(a, b) << std::endl;
+    std::cout << "Division is: " << division(a, b) << std::endl;
+    return 0;
+}
+
+double sum(double a, double b)
+{
+    return a + b;
+}
+
+double difference(double a, double b)
+{
+    return a - b;
+}
+
+double multiplication(double a, double b)
+{
+    return a * b;
+}
+
+double division(double a, double b)
+{
+    return a / b;
+}
