b2978a2a34da46fe9f05e2fccb97407883193b27
artoymdmitriev <admitriev5813@gmail.com>
2017-10-10 00:04:13 +0300

    Improve task6

--- hw2/task6/main.cpp
+++ hw2/task6/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 
-using namespace std;
+double min(double, double);
+double max(double, double);
+
+using std::cin;
+using std::cout;
+using std::endl;
 int main() {
-    int a, b, c;
+    double a, b, c;
     cout << "Enter 3 numbers: ";
     cin >> a >> b >> c;
     cout << "Max number is: " << max(min(a, b), c) << endl;
     system("pause");
     return 0;
+}
+
+double min(double num1, double num2) {
+    if(num1 < num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+double max(double num1, double num2) {
+    if(num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
