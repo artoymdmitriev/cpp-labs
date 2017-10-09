cf640bba4eb799b8b6618968152ffb6027a6ee04
artoymdmitriev <admitriev5813@gmail.com>
2017-10-10 00:56:10 +0300

    Improve task9

--- hw2/task9/main.cpp
+++ hw2/task9/main.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
-#include <cmath>
+
+double abs(double);
 
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main() {
-    double r1, r2, r3, x, y, absX, absY;
+    double r1, r2, r3, x, y;
     cout << "Enter r1, r2, r3: ";
     cin >> r1 >> r2 >> r3;
     cout << "Enter x, y: ";
     cin >> x >> y;
 
-    absX = fabs(x);
-    absY = fabs(y);
+    x = abs(x);
+    y = abs(y);
 
-    if((absX == r1 && absY == r1) ||
-            (absX == r2 && absY == r2) ||
-            (absX == r3 && absY == r3)) {
+    if((x * x + y * y == r1 * r1) ||
+            (x * x + y * y == r2 * r2) ||
+            (x * x + y * y == r3 * r3)) {
         cout << "The point is in the 5th part" << endl;
-    } else if(absX < r1 && absY < r1) {
+    } else if(x * x + y * y < r1 * r1) {
         cout << "The point is in the 1st part" << endl;
-    } else if(absX < r2 && absY < r2) {
+    } else if(x * x + y * y < r2 * r2) {
         cout << "The point is in the 2nd part" << endl;
-    } else if(absX < r3 && absY < r3) {
+    } else if(x * x + y * y < r3 * r3) {
         cout << "The point is in the 3rd part" << endl;
-    } else if(absX > r3 || absY > r3) {
+    } else {
         cout << "The point is in the 4th part" << endl;
     }
+
     system("pause");
     return 0;
+}
+
+double abs(double arg) {
+    if (arg < 0) {
+        return -arg;
+    } else {
+        return arg;
+    }
 }
