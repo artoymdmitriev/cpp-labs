1a347217a1fa26f15beea0cd9880ee13c14d2da1
artoymdmitriev <admitriev5813@gmail.com>
2017-10-13 00:20:09 +0300

    Improve task1

--- hw3/task1/main.cpp
+++ hw3/task1/main.cpp
@@ -2,6 +2,7 @@
 
 double max(double, double);
 double min(double, double);
+char get_answer();
 
 using std::cin;
 using std::cout;
@@ -9,12 +10,21 @@ using std::endl;
 
 int main() {
     while(true) {
-        double a, b, c;
-        cout << "Enter 3 numbers: ";
-        cin >> a >> b >> c;
+        char answer = get_answer();
+        if(answer == 'Y') {
+            double a, b, c;
+            cout << "Enter 3 numbers: ";
+            cin >> a >> b >> c;
 
-        cout << "Max value is: " << max(max(a, b), c) << endl;
-        cout << "Min value is: " << min(min(a, b), c) << endl;
+            cout << "Max value is: " << max(max(a, b), c) << endl;
+            cout << "Min value is: " << min(min(a, b), c) << endl;
+        } else if(answer == 'N') {
+            cout << "Goodbye!";
+            break;
+        } else {
+            cout << "Sorry, I don't understand you...";
+            continue;
+        }
     }
 }
 
@@ -24,4 +34,11 @@ double max(double num1, double num2) {
 
 double min(double num1, double num2) {
     return num1 < num2 ? num1 : num2;
+}
+
+char get_answer() {
+    char answer;
+    cout << "Do you want to continue? (Y/N)" << endl;
+    cin >> answer;
+    return answer;
 }
