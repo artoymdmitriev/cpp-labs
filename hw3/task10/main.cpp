5d1a5285d76dfcc94d0933126dbc9ce1837bd1f1
artoymdmitriev <admitriev5813@gmail.com>
2017-10-13 00:17:29 +0300

    Add task10

---
+++ hw3/task10/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+double abs(double);
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int k = 1;
+    double result, sum = 0, accuracy = 0.001;
+
+    do {
+        result = 1.0 / (k * k);
+        sum += result;
+        k++;
+    } while (abs(result) > accuracy);
+
+    cout << "Sum is: " << sum << endl << k;
+
+    return 0;
+}
+
+double abs(double num) {
+    return num < 0 ? -num : num;
+}
