225f3c1a8f0d685256161a905790af3926409402
artoymdmitriev <admitriev5813@gmail.com>
2017-10-12 21:45:42 +0300

    Add task3

---
+++ hw3/task3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int number;
+    cout << "Enter the number: ";
+    cin >> number;
+    int max = 0;
+    while(number > 0) {
+        max < number % 10 ? max = number % 10 : max;
+        number /= 10;
+    }
+    cout << "Max number is: " << max;
+    return 0;
+}
