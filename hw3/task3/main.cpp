ffc9461ea4c11c0b2af0d21e9433162785c8a51c
artoymdmitriev <admitriev5813@gmail.com>
2017-10-17 11:11:05 +0300

    Improve task3

--- hw3/task3/main.cpp
+++ hw3/task3/main.cpp
@@ -8,12 +8,17 @@ int main() {
     int number;
     cout << "Enter the number: ";
     cin >> number;
+    number = abs(number);
     int max = 0;
     while(number > 0) {
         max < number % 10 ? max = number % 10 : max;
         number /= 10;
     }
-    cout << "Max number is: " << max;
+    cout << "Max number is: " << max << endl;
     system("pause");
     return 0;
+}
+
+int abs(int num) {
+    return num < 0 ? -num : num;
 }
