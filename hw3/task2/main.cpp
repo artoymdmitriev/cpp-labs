fb6ded171705fbbd3baed9ffb9d902fa81fd2942
artoymdmitriev <admitriev5813@gmail.com>
2017-10-17 10:14:19 +0300

    Improve task2

--- hw3/task2/main.cpp
+++ hw3/task2/main.cpp
@@ -8,11 +8,17 @@ int main() {
     int number;
     cout << "Enter the number: ";
     cin >> number;
+    number = abs(number);
     int sum = 0;
     while(number > 0) {
         sum += number % 10;
         number /= 10;
     }
     cout << "Sum is: " << sum;
+    system("pause");
     return 0;
+}
+
+int abs(int num) {
+    return num < 0 ? -num : num;
 }
