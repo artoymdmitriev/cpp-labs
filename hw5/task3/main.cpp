1b1e03332783c4c301afba42d82e53687f0bbe5c
artoymdmitriev <admitriev5813@gmail.com>
2017-10-27 00:01:07 +0300

    Add task3

---
+++ hw5/task3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int get_year();
+bool is_leap_year(int);
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int year = get_year();
+    cout << (is_leap_year(year) ? 366 : 365);
+    cout << endl;
+    system("pause");
+    return 0;
+}
+
+int get_year() {
+    int year;
+    while(true) {
+        cout << "Enter year: " << endl;
+        cin >> year;
+        if(year > 0) break;
+        cout << "Wrong number, try again!" << endl;
+    }
+    return year;
+}
+
+bool is_leap_year(int year) {
+    bool result = false;
+    if(year % 4 == 0 && !(year % 100 == 0
+                          && year % 400 != 0)) result = true;
+    return result;
+}
