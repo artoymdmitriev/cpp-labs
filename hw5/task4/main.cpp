5719a854546b41cd917d4081f90d8bf808acd83c
artoymdmitriev <admitriev5813@gmail.com>
2017-10-26 23:37:21 +0300

    Split code into functions in task4

--- hw5/task4/main.cpp
+++ hw5/task4/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+void printAsc(int, int);
+void printDesc(int, int);
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -8,9 +11,17 @@ int main() {
     int a, b;
     cout << "Enter A and B" << endl;
     cin >> a >> b;
-    cout << "Asc" << endl;
-    for(int i = a + 1; i < b; i++) cout << i << endl;
-    cout << "Desc" << endl;
-    for(int i = b - 1; i > a; i--) cout << i << endl;
+    printAsc(a, b);
+    printDesc(a, b);
     return 0;
+}
+
+void printAsc(int a, int b) {
+    cout << "Asc:" << endl;
+    for(int i = a + 1; i < b; i++) cout << i << endl;
+}
+
+void printDesc(int a, int b) {
+    cout << "Desc:" << endl;
+    for(int i = b - 1; i > a; i--) cout << i << endl;
 }
