60af15642b4792b55e54a881238c2fbc3be71bcd
artoymdmitriev <admitriev5813@gmail.com>
2017-11-03 22:18:44 +0300

    Add task12

---
+++ hw6/task12/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void shiftLeft3(int&, int&, int&);
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    for(int i = 0; i < 2; i++) {
+        int a, b, c;
+        cout << "Enter 3 numbers: " << endl;
+        cin >> a >> b >> c;
+        shiftLeft3(a, b, c);
+        cout << "Result: " << a << " " << b << " " << c << endl;
+    }
+    system("pause");
+    return 0;
+}
+
+void shiftLeft3(int &a, int &b, int &c) {
+    int temp1 = c, temp2 = b;
+    c = a;
+    b = temp1;
+    a = temp2;
+}
