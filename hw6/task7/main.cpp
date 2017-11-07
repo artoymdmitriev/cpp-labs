55ff0c9fd3073221b1895818cb977e7a153a8ae2
artoymdmitriev <admitriev5813@gmail.com>
2017-11-07 16:58:49 +0300

    Rewrite task7 using references

--- hw6/task7/main.cpp
+++ hw6/task7/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-char changeRegister(char);
-char moveNums(char);
-char replaceOtherCharacters(char);
+void changeRegister(char&);
+void moveNums(char&);
+void replaceOtherCharacters(char&);
 
 using std::cin;
 using std::cout;
@@ -12,32 +12,29 @@ int main() {
     char ch;
     cout << "Enter a character: " << endl;
     cin >> ch;
-    ch = changeRegister(ch);
-    ch = moveNums(ch);
-    ch = replaceOtherCharacters(ch);
+    changeRegister(ch);
+    moveNums(ch);
+    replaceOtherCharacters(ch);
     cout << ch << endl;
     system("pause");
     return 0;
 }
 
-char changeRegister(char ch) {
+void changeRegister(char &ch) {
     int index = int(ch);
-    if(index >= 65 && index <= 90) return char(index + 32);
-    else if(index >= 97 && index <= 122) return char(index - 32);
-    else return ch;
+    if(index >= 65 && index <= 90) ch = char(index + 32);
+    else if(index >= 97 && index <= 122) ch = char(index - 32);
 }
 
-char moveNums(char ch) {
+void moveNums(char &ch) {
     int index = int(ch);
-    if(index >= 48 && index < 57) return char(++index);
-    else if(index == 57) return char('0');
-    else return ch;
+    if(index >= 48 && index < 57) ch = char(++index);
+    else if(index == 57) ch = '0';
 }
 
-char replaceOtherCharacters(char ch) {
+void replaceOtherCharacters(char &ch) {
     if(!(ch >= 'A' && ch <= 'Z')
        && !(ch >= 'a' && ch <= 'z')
        && !(ch >= '0' && ch <= '9'))
-        return '.';
-    else return ch;
+        ch = '.';
 }
