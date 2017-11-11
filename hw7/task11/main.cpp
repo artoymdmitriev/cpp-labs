0181c1fd3dcc20939d759dce5611f0b2d110dc23
artoymdmitriev <admitriev5813@gmail.com>
2017-11-11 21:09:05 +0300

    Add task11

---
+++ hw7/task11/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void shiftRight3(int*, int*, int*);
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    for(int i = 0; i < 2; i++) {
+        int a, b, c;
+        cout << "Enter 3 numbers: " << endl;
+        cin >> a >> b >> c;
+        shiftRight3(&a, &b, &c);
+        cout << "Result: " << a << " " << b << " " << c << endl;
+    }
+    system("pause");
+    return 0;
+}
+
+void shiftRight3(int *a, int *b, int *c) {
+    int temp1 = *a, temp2 = *b;
+    *a = *c;
+    *b = temp1;
+    *c = temp2;
+}
