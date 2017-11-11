f2fcd1d00fa3bc7dc92c671b752fbabed15ef40a
artoymdmitriev <admitriev5813@gmail.com>
2017-11-11 21:13:36 +0300

    Add task13

---
+++ hw7/task13/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+void incTime(int*, int*, int*, int*);
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int h, m, s, t;
+    while(true) {
+        cout << "Enter hours, minutes, seconds and T: " << endl;
+        cin >> h >> m >> s >> t;
+        if(h < 0 || m < 0 || s < 0 || t < 0) {
+            cout << "All numbers must be positive! Try again." << endl;
+        } else if(h > 23 || m > 59 || s > 59) {
+            cout << "Your time is incorrect! Try again." << endl;
+        } else {
+            break;
+        }
+    }
+    incTime(&h, &m, &s, &t);
+    cout << h << ":" << m << ":" << s << endl;
+    system("pause");
+    return 0;
+}
+
+void incTime(int *h, int *m, int *s, int *t) {
+    *s += *t;
+
+    while(*s > 59) {
+        *m += 1;
+        *s -= 60;
+    }
+
+    while(*m > 59) {
+        *h += 1;
+        *m -= 60;
+    }
+
+    while(*h > 23) {
+        *h -= 24;
+    }
+}
