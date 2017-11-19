a3538d28ab442e4fea63aebfab3f9fd62527e7f9
artoymdmitriev <admitriev5813@gmail.com>
2017-11-19 20:37:34 +0300

    Add task7

---
+++ hw8/task7/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void printMatrix(int*, int, int);
+void fillMatrix(int*, int, int);
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    const int rows = 2;
+    const int columns = 3;
+    int array[rows][columns];
+    fillMatrix(array[0], rows, columns);
+    printMatrix(array[0], rows, columns);
+    system("pause");
+    return 0;
+}
+
+void fillMatrix(int* array, int rows, int columns) {
+    cout << "Fill in the " << rows << "x" << columns << " matrix" << endl;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            cout << "array[" << i << "][" << j << "] = ";
+            cin >> *(array + i * columns + j);
+        }
+    }
+}
+
+void printMatrix(int *array, int rows, int columns) {
+    cout << "Result: " << endl;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            cout << *(array + i * columns + j) << " ";
+        }
+        cout << endl;
+    }
+}
