389db3973aafb7a26faf2be2aad0a4d993f9ed49
artoymdmitriev <admitriev5813@gmail.com>
2017-12-11 02:54:21 +0300

    Add task2

--- hw11/task2/main.cpp
+++ hw11/task2/main.cpp
@@ -1,14 +1,19 @@
+//2. Напишите функцию для изменения длины ДРМ, которая
+//1) выделяет новый участок памяти подходящей длины
+//2) копирует в него данные из старого участка. Если данных не хватает, то заполняет оставшийся участок нулями
+//3) освобождает старый участок памяти
+
 #include <iostream>
 #include <ctime>
 
-int firstLength;
-int secondLength;
+int* createNewArray(int*, int, int);
 
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main() {
+    int firstLength;
     cout << "Enter the length of the first array: ";
     cin >> firstLength;
     int *array = new int[firstLength];
@@ -16,24 +21,31 @@ int main() {
     for(int i = 0; i < firstLength; i++) {
         cin >> array[i];
     }
+
+    int secondLength;
     cout << "Enter the length of the second array: ";
     cin >> secondLength;
 
-    int *newArray = new int[secondLength];
+    int *newArray = createNewArray(array, firstLength, secondLength);
+    cout << "Second array: ";
     for(int i = 0; i < secondLength; i++) {
-        if(i < firstLength) {
+        cout << newArray[i] << ' ';
+    }
+    cout << endl;
+    delete[] array;
+
+    system("pause");
+    return 0;
+}
+
+int* createNewArray(int* array, int oldArrayLength, int newArrayLength) {
+    int *newArray = new int[newArrayLength];
+    for(int i = 0; i < newArrayLength; i++) {
+        if(i < oldArrayLength) {
             newArray[i] = array[i];
         } else {
             newArray[i] = 0;
         }
     }
-
-    cout << "Second array: ";
-    for(int i = 0; i < secondLength; i++) {
-        cout << newArray[i] << ' ';
-    }
-    delete[] array;
-    
-    system("pause");
-    return 0;
+    return newArray;
 }
