c1779100675c900ec08fad65d642cf110c4a7c2b
artoymdmitriev <admitriev5813@gmail.com>
2017-12-15 23:50:20 +0300

    Revert "Add task1 OOP"
    This reverts commit ffebd0b57ae3db224579ea2ecedfdfa90bf6d8d0.

--- hw10/task1/main.cpp
+++ hw10/task1/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-const int rows = 4;
-const int columns = 4;
+const int columns = 3;
+const int rows = 2;
 
-void fillMatrixRows(int [][columns]);
-void fillMatrixColumns(int [][columns]);
-void fillMatrixCorners(int [][columns]);
-void fillMatrixSnake(int [][columns]);
-void printMatrix(int [][columns]);
+void fillMatrix(int [][columns]);
+void printMatrixRows(int [][columns]);
+void printMatrixColumns(int [][columns]);
 
 using std::cin;
 using std::cout;
@@ -15,21 +13,15 @@ using std::endl;
 
 int main() {
     int matrix[rows][columns];
-    //fillMatrixRows(matrix);
-    //printMatrix(matrix);
-    //cout << endl;
-    //fillMatrixSnake(matrix);
-    //fillMatrixColumns(matrix);
-    //printMatrix(matrix);
-    //cout << endl;
-
-    fillMatrixCorners(matrix);
-    system("pause");
+    fillMatrix(matrix);
+    printMatrixRows(matrix);
+    cout << endl;
+    printMatrixColumns(matrix);
+    cout << endl;
     return 0;
 }
 
-
-void fillMatrixRows(int matrix[][columns]) {
+void fillMatrix(int matrix[][columns]) {
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
             cin >> matrix[i][j];
@@ -37,26 +29,19 @@ void fillMatrixRows(int matrix[][columns]) {
     }
 }
 
-void fillMatrixColumns(int matrix[][columns]) {
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < columns; j++) {
-            cin >> matrix[j][i];
-        }
-    }
-}
-
-void fillMatrixCorners(int matrix[][columns]) {
-
-}
-
-void fillMatrixSnake(int matrix[][columns]) {
-
-}
-
-void printMatrix(int matrix[][columns]) {
+void printMatrixRows(int matrix[][columns]) {
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
             cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void printMatrixColumns(int matrix[][columns]) {
+    for(int i = 0; i < columns; i++) {
+        for(int j = 0; j < rows; j++) {
+            cout << matrix[j][i] << " ";
         }
         cout << endl;
     }
